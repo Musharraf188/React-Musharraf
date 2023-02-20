38d4ef9cb2185c8946dc869cb5ed6a5b548fa3e5
Musharraf <musharrafsayyad1@gmail.com>
2023-02-20 09:38:47 +0530

    Angular

--- day02/Angular__Vs__React.js
+++ day02/Angular__Vs__React.js
@@ -1,5 +1,5 @@
 //what is major diffrence between angular and React ??
-//why we use react instred of Angular ?? because we made same application in Angular also so why using Angular ??
+//why we use react instred of Angular ?? because we made same application in Angular also, so why using Angular ??
 /*Difference Between React and Other Framework or Lib (Angular-Google)?(MEAN Stack)
 MERN Stack)-Full Stack -- html,css,javascript,react,angular-frontend developer?
 React -lib and Angular -framework-Frameworks comes with all predefined features like testiblity, API Calls etc. In build. But with react we need to decide
