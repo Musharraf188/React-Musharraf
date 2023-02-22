59eeab9b92beaf14987d4dbb2f50d5d63c3fafe9
Musharraf <musharrafsayyad1@gmail.com>
2023-02-22 11:27:13 +0530

    Rest_spread operater

---
+++ day06/rest_spread.js
@@ -0,0 +1,28 @@
+const numbers = [2, 4, 5, 7];
+// Rest Operator (...)
+const [first, second, ...restItems] = numbers;
+
+console.log("First & second", first, second );
+console.log("newArray:", restItems);
+const obj1 = {
+  temp1: "34",
+  fName: "Azhar",
+  lName: "Shaikh",
+  height: 5.9,
+  temp2: "546",
+};
+
+const { temp1, temp2, ...person } = obj1;
+
+console.log(person);
+
+// Spread Operator----   creates a new copy of object & array
+const obj2= { ...obj1 };
+obj2.color = "Fair";
+
+console.log("obj2", obj2);
+console.log("Obj1", obj1);
+
+const newArray = [...numbers];
+newArray.push(8);
+console.log("newArray", newArray);
